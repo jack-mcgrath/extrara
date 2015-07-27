85d025749bdc0b2ec523ba7c9397c8eca35a1668
jack-mcgrath <jackmcgrath7@gmail.com>
2015-07-26 20:04:00 -0700

    font

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,7 +2,7 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
-Rails.application.config.assets.precompile += ['bootstrap.min.js', 'grayscale.css', 'grayscale.js', 'js.js', 'stats.js', 'jquery.js', 'staticindex.css', 'staticindex2.css', 'common.js', 'main.js', 'util.js', 'font-awesome.min.css', 'grayscale.css', 'bootstrap.min.css']
+Rails.application.config.assets.precompile += ['bootstrap.min.js', 'grayscale.css', 'grayscale.js', 'js.js', 'stats.js', 'jquery.js', 'staticindex.css', 'staticindex2.css', 'common.js', 'main.js', 'util.js', 'font-awesome.min.css', 'grayscale.css', 'bootstrap.min.css', 'font-awesome.min.css']
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
