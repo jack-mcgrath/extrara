715d0ed4b67995216e738b2218c2e1d1855fa92c
jack-mcgrath <jackmcgrath7@gmail.com>
2015-12-12 18:30:39 -0500

    ASDF

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,8 +2,8 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
-Rails.application.config.assets.precompile += ['bootstrap.min.js', 'grayscale.css', 'grayscale.js', 'js.js', 'stats.js', 'jquery.js', 'staticindex.css', 'staticindex2.css', 'common.js', 'main.js', 'util.js', 'font-awesome.min.css', 'grayscale.css', 'bootstrap.min.css', 'font-awesome.min.css', 'forem.css', 'forem.js']
-Rails.application.config.assets.precompile += [/.*\.js/,/.*\.css/]
+#Rails.application.config.assets.precompile += ['bootstrap.min.js', 'grayscale.css', 'grayscale.js', 'js.js', 'stats.js', 'jquery.js', 'staticindex.css', 'staticindex2.css', 'common.js', 'main.js', 'util.js', 'font-awesome.min.css', 'grayscale.css', 'bootstrap.min.css', 'font-awesome.min.css', 'forem.css', 'forem.js']
+#Rails.application.config.assets.precompile += [/.*\.js/,/.*\.css/]
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
